24bce273d1f31bdfd169592b9ecf3c034482a394
Nicolas Muñoz <nicofermunlug@gmail.com>
2025-05-07 15:49:28 -0500

    hotfix - configuro compatibilidad con github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Lenos-de-Tabi/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
