f58d70307dbf0d29995e486b984765c893627480
NicoFerMunLug <nicofermunlug@gmail.com>
2025-05-07 21:23:45 -0500

    hotfix - ajusto base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// vite.config.ts
+// filepath: c:\Users\nicolas.munoz\Downloads\Proyecto cafe\Lenos-de-Tabi\vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
 })
