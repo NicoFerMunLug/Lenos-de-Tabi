be9daaf8ba56430ec3aac54c139e774fa94d69a6
Nicolas Muñoz <nicofermunlug@gmail.com>
2025-05-07 20:01:44 -0500

    Revert "hotfix - segunda versión ajuste github pages"
    This reverts commit 2fc6887d9673916047c0b8b73819d564a5f55097.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Lenos-de-Tabi/',
   plugins: [react()],
-});
+})
